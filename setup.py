79e8393bd584ef6b51ac83d267e2830ca5613dde
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-10-16 18:21:47 -0400

    Updated setup.py for publishing.

--- setup.py
+++ setup.py
@@ -19,7 +19,14 @@ setup(
     description="TestFlows - Connect",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
-    url="http://testflows.com",
+    url="https://github.com/testflows/testflows-connect",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
     license="Apache-2.0",
     packages=[
         "testflows.connect",
