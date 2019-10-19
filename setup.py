c6cbb4349261f89a7274071ba803bdc61f24695c
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-10-19 08:08:21 -0400

    Updates.

--- setup.py
+++ setup.py
@@ -13,12 +13,17 @@
 # limitations under the License.
 from setuptools import setup
 
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
 setup(
     name="testflows.connect",
     version="__VERSION__",
     description="TestFlows - Connect",
     author="Vitaliy Zakaznikov",
     author_email="vzakaznikov@testflows.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/testflows/testflows-connect",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
