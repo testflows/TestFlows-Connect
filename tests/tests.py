538e0b68c2ec45c3476e7fe2b259ac7c266fe815
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2020-06-16 21:39:35 -0400

    Moving to 1.6 core.

--- tests/tests.py
+++ tests/tests.py
@@ -16,98 +16,95 @@
 from testflows.core import *
 from testflows.asserts import error, values
 
-class RegressionSuite(TestBase):
-    description = """
-    Suite of Shell tests.
+@TestSuite
+def suite(self):
+    """Suite of Shell tests.
     """
-    def run(self):
-        with Test("import"):
-            from testflows.connect import Shell
+    with Test("import"):
+        from testflows.connect import Shell
 
-        with Test("open"):
-            with Shell() as bash:
-                pass
-
-        with Test("execute command"):
-            with Shell() as bash:
-                bash("ls -la")
-
-        with Test("custom shell name"):
-            with Shell(name="shell") as shell:
-                shell("ls -la")
-
-        with Test("execute command with custom name"):
-            with Shell() as bash:
-                bash("ls -la", name="ls")
-
-        with Test("execute multiple commands"):
-            with Shell() as bash:
-                bash("echo Hello World")
-                bash("ls -la")
-                bash("echo Bye World")
-
-        with Test("share the same shell between different tests"):
-            with Shell() as bash:
-                with Step("first test"):
-                    bash("echo Hello World")
-                with Step("second test"):
-                    bash("ls -la")
-                with Step("third test"):
-                    bash("echo Bye World")
-
-        with Test("check command output"):
-            with Shell() as bash:
-                with Step("one line output"):
-                    assert bash("echo Hello World").output == "Hello World", error()
-                with Step("empty output"):
-                    assert bash("echo ").output == "", error()
-                with Step("multi line output"):
-                    text = "line1\\nline2"
-                    with values() as that:
-                        assert that(bash(f"echo -e \"{text}\"").output) == text.replace("\\n", "\n"), error()
-
-        with Test("check command exitcode"):
-            with Shell() as bash:
-                with Step("exit code 0"):
-                    assert bash("ls -la").exitcode == 0, error()
-                with Step("exit code 2"):
-                    assert bash("ls /foo__").exitcode == 2, error()
-
-        with Test("check timeout"):
-            with Shell() as bash:
-                bash.timeout = 6
-                with Step("timeout 1 sec"):
-                    bash("echo hello; sleep 0.75; " * 5)
-
-        with Test("async command"):
-            with Shell() as bash:
-                with bash("tail -f /proc/cpuinfo", asyncronous=True) as tail:
-                    tail.readlines()
-
-        with Test("async command with custom name"):
-            with Shell() as bash:
-                with bash("tail -f /proc/cpuinfo", asyncronous=True, name="cpuinfo") as tail:
-                    tail.readlines()
-
-        with Test("check double prompts before command"):
-            with Shell() as bash:
-                bash.send("")
-                bash("ls")
-
-        with Test("check double prompts after command"):
-            with Shell() as bash:
-                bash.send("")
-                bash("ls\r\n")
-                bash("ls; echo -e 'bash# \nbash# '")
-
-class Regression(TestBase):
-    def run(self):
-        with Test("import testflows.connect"):
-            import testflows.connect
-
-        with Test("suite", test=RegressionSuite):
+    with Test("open"):
+        with Shell() as bash:
             pass
 
+    with Test("execute command"):
+        with Shell() as bash:
+            bash("ls -la")
+
+    with Test("custom shell name"):
+        with Shell(name="shell") as shell:
+            shell("ls -la")
+
+    with Test("execute command with custom name"):
+        with Shell() as bash:
+            bash("ls -la", name="ls")
+
+    with Test("execute multiple commands"):
+        with Shell() as bash:
+            bash("echo Hello World")
+            bash("ls -la")
+            bash("echo Bye World")
+
+    with Test("share the same shell between different tests"):
+        with Shell() as bash:
+            with Step("first test"):
+                bash("echo Hello World")
+            with Step("second test"):
+                bash("ls -la")
+            with Step("third test"):
+                bash("echo Bye World")
+
+    with Test("check command output"):
+        with Shell() as bash:
+            with Step("one line output"):
+                assert bash("echo Hello World").output == "Hello World", error()
+            with Step("empty output"):
+                assert bash("echo ").output == "", error()
+            with Step("multi line output"):
+                text = "line1\\nline2"
+                with values() as that:
+                    assert that(bash(f"echo -e \"{text}\"").output) == text.replace("\\n", "\n"), error()
+
+    with Test("check command exitcode"):
+        with Shell() as bash:
+            with Step("exit code 0"):
+                assert bash("ls -la").exitcode == 0, error()
+            with Step("exit code 2"):
+                assert bash("ls /foo__").exitcode == 2, error()
+
+    with Test("check timeout"):
+        with Shell() as bash:
+            bash.timeout = 6
+            with Step("timeout 1 sec"):
+                bash("echo hello; sleep 0.75; " * 5)
+
+    with Test("async command"):
+        with Shell() as bash:
+            with bash("tail -f /proc/cpuinfo", asyncronous=True) as tail:
+                tail.readlines()
+
+    with Test("async command with custom name"):
+        with Shell() as bash:
+            with bash("tail -f /proc/cpuinfo", asyncronous=True, name="cpuinfo") as tail:
+                tail.readlines()
+
+    with Test("check double prompts before command"):
+        with Shell() as bash:
+            bash.send("")
+            bash("ls")
+
+    with Test("check double prompts after command"):
+        with Shell() as bash:
+            bash.send("")
+            bash("ls\r\n")
+            bash("ls; echo -e 'bash# \nbash# '")
+
+@TestModule
+def regression(self):
+   with Test("import testflows.connect"):
+       import testflows.connect
+
+   Suite(run=suite)
+
 if main():
-    with Test("regression", test=Regression):
-        pass
+    Module(run=regression)
